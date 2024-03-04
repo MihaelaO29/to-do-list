2c1d780d805fc7e787ae9a91fec5d198c9ce1226
MihaelaO29 <opriamihaela22@yahoo.com>
2024-03-04 16:18:37 +0200

    added all components and style for them

---
+++ src/utils/content/content.js
@@ -0,0 +1,15 @@
+import './content.css';
+
+const Content = () => {
+    return (
+        <div className='content'>
+            <div className='activity-list'>
+                Activity
+            </div>
+            <div className='button'> + New Task</div>
+        </div>
+    )
+}
+
+
+export default Content;
---
+++ src/utils/header/header.js
@@ -0,0 +1,13 @@
+import './header.css';
+
+const Header = () => {
+    return (
+        <div className='header'>
+        <div className='title'>
+            To Do List
+        </div>
+        </div>
+    )
+}
+
+export default Header;
